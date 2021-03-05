ec72e4c694ce348129b73a0dc1c85f4526bd135d
Madhu Rajanna <madhupr007@gmail.com>
2021-03-05 12:06:20 +0530

    grpc: added required GRPC helper functions for replication
    added required GRPC wrappers to make call to the CSI
    driver.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

---
+++ pkg/client/client.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/kubernetes-csi/csi-lib-utils/connection"
+	"github.com/kubernetes-csi/csi-lib-utils/metrics"
+	"github.com/kubernetes-csi/csi-lib-utils/rpc"
+	"google.golang.org/grpc"
+)
+
+// Client holds the GRPC connenction details
+type Client struct {
+	client  *grpc.ClientConn
+	timeout time.Duration
+}
+
+// Connect to the GRPC client
+func connect(address string) (*grpc.ClientConn, error) {
+	return connection.Connect(address, metrics.NewCSIMetricsManager(""), connection.OnConnectionLoss(connection.ExitOnConnectionLoss()))
+}
+
+// New creates and returns the GRPC client
+func New(address string, timeout time.Duration) (*Client, error) {
+	c := &Client{}
+	cc, err := connect(address)
+	if err != nil {
+		return c, err
+	}
+	c.client = cc
+	c.timeout = timeout
+	return c, nil
+}
+
+// Probe the GRPC client once
+func (c *Client) Probe() error {
+	return rpc.ProbeForever(c.client, c.timeout)
+}
+
+// GetDriverName gets the driver name from the driver
+func (c *Client) GetDriverName() (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+	return rpc.GetDriverName(ctx, c.client)
+}
---
+++ pkg/replication/replication-client.go
@@ -0,0 +1,122 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package replication
+
+import (
+	"context"
+	"time"
+
+	replicationlib "github.com/kube-storage/spec/lib/go/replication"
+	"google.golang.org/grpc"
+)
+
+type replicationClient struct {
+	client  replicationlib.ControllerClient
+	timeout time.Duration
+}
+
+// VolumeReplication holds the methods requried for volume replication.
+type VolumeReplication interface {
+	// EnableVolumeReplication RPC call to enable the volume replication.
+	EnableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error)
+	// DisableVolumeReplication RPC call to disable the volume replication.
+	DisableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error)
+	// PromoteVolume RPC call to promote the volume.
+	PromoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.
+		PromoteVolumeResponse, error)
+	// DemoteVolume RPC call to demote the volume.
+	DemoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.
+		DemoteVolumeResponse, error)
+	// ResyncVolume RPC call to resync the volume.
+	ResyncVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.
+		ResyncVolumeResponse, error)
+}
+
+// NewReplicationClient returns VolumeReplication interface which has the RPC
+// calls for replication.
+func NewReplicationClient(cc *grpc.ClientConn, timeout time.Duration) VolumeReplication {
+	return &replicationClient{client: replicationlib.NewControllerClient(cc), timeout: timeout}
+}
+
+// EnableVolumeReplication RPC call to enable the volume replication.
+func (rc *replicationClient) EnableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error) {
+	req := &replicationlib.EnableVolumeReplicationRequest{
+		VolumeId:   volumeID,
+		Parameters: parameters,
+		Secrets:    secrets,
+	}
+
+	createCtx, cancel := context.WithTimeout(context.Background(), rc.timeout)
+	defer cancel()
+	resp, err := rc.client.EnableVolumeReplication(createCtx, req)
+	return resp, err
+}
+
+// DisableVolumeReplication RPC call to disable the volume replication.
+func (rc *replicationClient) DisableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error) {
+	req := &replicationlib.DisableVolumeReplicationRequest{
+		VolumeId:   volumeID,
+		Parameters: parameters,
+		Secrets:    secrets,
+	}
+
+	createCtx, cancel := context.WithTimeout(context.Background(), rc.timeout)
+	defer cancel()
+	resp, err := rc.client.DisableVolumeReplication(createCtx, req)
+	return resp, err
+}
+
+// PromoteVolume RPC call to promote the volume.
+func (rc *replicationClient) PromoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.PromoteVolumeResponse, error) {
+	req := &replicationlib.PromoteVolumeRequest{
+		VolumeId:   volumeID,
+		Parameters: parameters,
+		Secrets:    secrets,
+	}
+
+	createCtx, cancel := context.WithTimeout(context.Background(), rc.timeout)
+	defer cancel()
+	resp, err := rc.client.PromoteVolume(createCtx, req)
+	return resp, err
+}
+
+// DemoteVolume RPC call to demote the volume.
+func (rc *replicationClient) DemoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DemoteVolumeResponse, error) {
+	req := &replicationlib.DemoteVolumeRequest{
+		VolumeId:   volumeID,
+		Parameters: parameters,
+		Secrets:    secrets,
+	}
+	createCtx, cancel := context.WithTimeout(context.Background(), rc.timeout)
+	defer cancel()
+	resp, err := rc.client.DemoteVolume(createCtx, req)
+	return resp, err
+}
+
+// ResyncVolume RPC call to resync the volume.
+func (rc *replicationClient) ResyncVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.ResyncVolumeResponse, error) {
+	req := &replicationlib.ResyncVolumeRequest{
+		VolumeId:   volumeID,
+		Parameters: parameters,
+		Secrets:    secrets,
+	}
+
+	createCtx, cancel := context.WithTimeout(context.Background(), rc.timeout)
+	defer cancel()
+	resp, err := rc.client.ResyncVolume(createCtx, req)
+	return resp, err
+}
