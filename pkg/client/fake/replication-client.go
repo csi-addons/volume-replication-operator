1d7df3dfcb2d58acd723b8eb798865c139a1a30b
Madhu Rajanna <madhupr007@gmail.com>
2021-03-08 18:44:38 +0530

    test: add mock client for replication
    added mock client for replication, mocking GRPC
    client helps to unit test the operator.
    
    added unit testing for the Replication operation with
    the help of the mock client.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

---
+++ pkg/client/fake/replication-client.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+import (
+	"context"
+
+	replicationlib "github.com/kube-storage/spec/lib/go/replication"
+)
+
+// ReplicationClient to fake replication operations.
+type ReplicationClient struct {
+	// EnableVolumeReplicationMock mocks EnableVolumeReplication RPC call.
+	EnableVolumeReplicationMock func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error)
+	// DisableVolumeReplicationMock mocks DisableVolumeReplication RPC call.
+	DisableVolumeReplicationMock func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error)
+	// PromoteVolumeMock mocks PromoteVolume RPC call.
+	PromoteVolumeMock func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.PromoteVolumeResponse, error)
+	// DemoteVolumeMock mocks DemoteVolume RPC call.
+	DemoteVolumeMock func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DemoteVolumeResponse, error)
+	// ResyncVolumeMock mocks ResyncVolume RPC call.
+	ResyncVolumeMock func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.ResyncVolumeResponse, error)
+}
+
+// EnableVolumeReplication calls EnableVolumeReplicationMock mock function.
+func (rc *ReplicationClient) EnableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error) {
+	return rc.EnableVolumeReplicationMock(ctx, volumeID, secrets, parameters)
+}
+
+// DisableVolumeReplication calls DisableVolumeReplicationMock mock function.
+func (rc *ReplicationClient) DisableVolumeReplication(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error) {
+	return rc.DisableVolumeReplicationMock(ctx, volumeID, secrets, parameters)
+}
+
+// PromoteVolume calls PromoteVolumeMock mock function.
+func (rc *ReplicationClient) PromoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.PromoteVolumeResponse, error) {
+	return rc.PromoteVolumeMock(ctx, volumeID, secrets, parameters)
+}
+
+// DemoteVolume calls DemoteVolumeMock mock function.
+func (rc *ReplicationClient) DemoteVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DemoteVolumeResponse, error) {
+	return rc.DemoteVolumeMock(ctx, volumeID, secrets, parameters)
+}
+
+// ResyncVolume calls ResyncVolumeMock function.
+func (rc *ReplicationClient) ResyncVolume(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.ResyncVolumeResponse, error) {
+	return rc.ResyncVolumeMock(ctx, volumeID, secrets, parameters)
+}
