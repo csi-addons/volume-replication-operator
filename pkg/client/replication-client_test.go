f966386c4efb18c7ae497f5eafa0b2f7523ac309
Madhu Rajanna <madhupr007@gmail.com>
2021-03-08 18:44:38 +0530

    test: add mock client for replication
    added mock client for replication, mocking GRPC
    client helps to unit test the operator.
    
    added unit testing for the Replication operation with
    the help of the mock client.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

---
+++ pkg/client/replication-client_test.go
@@ -0,0 +1,155 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	replicationlib "github.com/kube-storage/spec/lib/go/replication"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+
+	"github.com/kube-storage/volume-replication-operator/pkg/client/fake"
+)
+
+func TestEnableVolumeReplication(t *testing.T) {
+	var client = NewReplicationClient(&grpc.ClientConn{}, time.Minute)
+	// return success response
+	mockedEnableReplication := &fake.ReplicationClient{
+		EnableVolumeReplicationMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error) {
+			return &replicationlib.EnableVolumeReplicationResponse{}, nil
+		},
+	}
+	client = mockedEnableReplication
+	resp, err := client.EnableVolumeReplication(context.Background(), "", nil, nil)
+	assert.Equal(t, &replicationlib.EnableVolumeReplicationResponse{}, resp)
+	assert.Nil(t, err)
+
+	// return error
+	mockedEnableReplication = &fake.ReplicationClient{
+		EnableVolumeReplicationMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.EnableVolumeReplicationResponse, error) {
+			return nil, errors.New("failed to enable mirroring")
+		},
+	}
+	client = mockedEnableReplication
+	resp, err = client.EnableVolumeReplication(context.Background(), "", nil, nil)
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+}
+
+func TestDisableVolumeReplication(t *testing.T) {
+	var client = NewReplicationClient(&grpc.ClientConn{}, time.Minute)
+	// return success response
+	mockedDisableReplication := &fake.ReplicationClient{
+		DisableVolumeReplicationMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error) {
+			return &replicationlib.DisableVolumeReplicationResponse{}, nil
+		},
+	}
+	client = mockedDisableReplication
+	resp, err := client.DisableVolumeReplication(context.Background(), "", nil, nil)
+	assert.Equal(t, &replicationlib.DisableVolumeReplicationResponse{}, resp)
+	assert.Nil(t, err)
+
+	// return error
+	mockedDisableReplication = &fake.ReplicationClient{
+		DisableVolumeReplicationMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DisableVolumeReplicationResponse, error) {
+			return nil, errors.New("failed to disable mirroring")
+		},
+	}
+	client = mockedDisableReplication
+	resp, err = client.DisableVolumeReplication(context.Background(), "", nil, nil)
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+}
+
+func TestPromoteVolume(t *testing.T) {
+	var client = NewReplicationClient(&grpc.ClientConn{}, time.Minute)
+	// return success response
+	mockedPromoteVolume := &fake.ReplicationClient{
+		PromoteVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.PromoteVolumeResponse, error) {
+			return &replicationlib.PromoteVolumeResponse{}, nil
+		},
+	}
+	client = mockedPromoteVolume
+	resp, err := client.PromoteVolume(context.Background(), "", nil, nil)
+	assert.Equal(t, &replicationlib.PromoteVolumeResponse{}, resp)
+	assert.Nil(t, err)
+
+	// return error
+	mockedPromoteVolume = &fake.ReplicationClient{
+		PromoteVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.PromoteVolumeResponse, error) {
+			return nil, errors.New("failed to promote volume")
+		},
+	}
+	client = mockedPromoteVolume
+	resp, err = client.PromoteVolume(context.Background(), "", nil, nil)
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+}
+
+func TestDemoteVolume(t *testing.T) {
+	var client = NewReplicationClient(&grpc.ClientConn{}, time.Minute)
+	// return success response
+	mockedDemoteVolume := &fake.ReplicationClient{
+		DemoteVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DemoteVolumeResponse, error) {
+			return &replicationlib.DemoteVolumeResponse{}, nil
+		},
+	}
+	client = mockedDemoteVolume
+	resp, err := client.DemoteVolume(context.Background(), "", nil, nil)
+	assert.Equal(t, &replicationlib.DemoteVolumeResponse{}, resp)
+	assert.Nil(t, err)
+
+	// return error
+	mockedDemoteVolume = &fake.ReplicationClient{
+		DemoteVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.DemoteVolumeResponse, error) {
+			return nil, errors.New("failed to demote volume")
+		},
+	}
+	client = mockedDemoteVolume
+	resp, err = client.DemoteVolume(context.Background(), "", nil, nil)
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+}
+
+func TestResyncVolume(t *testing.T) {
+	var client = NewReplicationClient(&grpc.ClientConn{}, time.Minute)
+	// return success response
+	mockedResyncVolume := &fake.ReplicationClient{
+		ResyncVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.ResyncVolumeResponse, error) {
+			return &replicationlib.ResyncVolumeResponse{}, nil
+		},
+	}
+	client = mockedResyncVolume
+	resp, err := client.ResyncVolume(context.Background(), "", nil, nil)
+	assert.Equal(t, &replicationlib.ResyncVolumeResponse{}, resp)
+	assert.Nil(t, err)
+
+	// return error
+	mockedResyncVolume = &fake.ReplicationClient{
+		ResyncVolumeMock: func(ctx context.Context, volumeID string, secrets, parameters map[string]string) (*replicationlib.ResyncVolumeResponse, error) {
+			return nil, errors.New("failed to resync volume")
+		},
+	}
+	client = mockedResyncVolume
+	resp, err = client.ResyncVolume(context.Background(), "", nil, nil)
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+}
